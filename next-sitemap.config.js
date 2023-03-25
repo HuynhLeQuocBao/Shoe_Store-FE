3c52311aa7dac65132a2477f04514c6837639fde
itcgroup <itcgroup@joblogic.com>
2023-03-25 19:55:46 +0700

    add sitemap for website

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "http://shoe-store-fe.vercel.app/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
