38dd099e839743f1e278eef4e2b60c8b21bb6233
dao phan <daophan.pix@gmail.com>
2023-04-09 10:33:44 +0700

    change height loading

--- components/section/loading/LoadingProduct.jsx
+++ components/section/loading/LoadingProduct.jsx
@@ -11,7 +11,7 @@ const LoadingProduct = ({ numberOfCards, className }) => {
       {data.map((item) => (
         <div className="w-full" key={item}>
           <div className="mb-7 flex flex-col items-center justify-center md:mb-0 shadow-product-line p-4">
-            <div className="mb-2 bg-loading-gradient h-60 min-w-full"></div>
+            <div className="mb-2 bg-loading-gradient h-52 min-w-full"></div>
             <div className="text-center flex-col font-Rokkitt text-lg w-full h-[90px] flex-center">
               <p className="mb-2 text-ellipsis bg-loading-gradient h-14 min-w-full"></p>
               <p className=" bg-loading-gradient h-10 min-w-full"></p>
