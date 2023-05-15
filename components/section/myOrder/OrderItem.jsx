4ba9e8c41ca393cf5e3cd1211138db29a215580f
itcgroup <itcgroup@joblogic.com>
2023-05-15 17:49:19 +0700

    responsive list order

--- components/section/myOrder/OrderItem.jsx
+++ components/section/myOrder/OrderItem.jsx
@@ -28,7 +28,7 @@ const OrderItem = ({ data, stateOrder, index }) => {
     if (checkAddReview) setIsRated(true);
   }, [checkAddReview]);
   return (
-    <>
+    <div className="px-4 md:px-0">
       <div className="w-full text-sm hidden md:grid grid-cols-12  border border-b-2 shadow-lg rounded-3xl duration-500 py-3 mb-2">
         <div className="text-center col-span-1 flex justify-center items-center">
           <span>{index + 1}</span>
@@ -56,7 +56,7 @@ const OrderItem = ({ data, stateOrder, index }) => {
         </div>
         <div className="col-span-3 md:col-span-2 text-center flex justify-center items-center">
           <span>
-            {moment(data?.updatedAt).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+            {moment(data?.updatedAt).format("dddd, MMMM Do YYYY, h:mm:ss")}
           </span>
         </div>
         <div className="col-span-2  flex justify-center items-center">
@@ -81,11 +81,11 @@ const OrderItem = ({ data, stateOrder, index }) => {
           </Link>
         </div>
       </div>
-      <div className="w-full h-full flex md:hidden flex-col gap-10 shadow-lg mb-6 py-2 rounded-lg m-2">
-        <div className=" grid  grid-cols-12  ">
-          <div
-            className={` 
-                col-span-4           
+      <div className="bg-slate-100 w-full h-full flex md:hidden flex-col shadow-lg py-2 rounded-lg mb-6">
+        <div className="flex items-center justify-between">
+          <div className="pl-2 h-full">
+            <div
+              className={`    
                 ${
                   data.status === 0
                     ? "bg-orange-400"
@@ -95,19 +95,20 @@ const OrderItem = ({ data, stateOrder, index }) => {
                     ? "bg-slate-400"
                     : "bg-blue-400"
                 }
-                w-32 h-32 p-2  uppercase text-white font-bold flex items-center justify-center
+                uppercase text-white font-bold flex items-center justify-center py-2 px-3 rounded-lg items-start
             `}
-          >
-            {changeState(data.status)}
+            >
+              {changeState(data.status)}
+            </div>
           </div>
-          <div className="col-span-6 flex flex-col gap-2 px-2">
+          <div className="flex flex-col gap-2 px-5">
             <div className="">
               <b>Total: </b>
               {data.total}
             </div>
             <span className="">
               <b>Date: </b>
-              {moment(data?.updatedAt).format("dddd, MMMM Do YYYY, h:mm:ss a")}
+              {moment(data?.updatedAt).format("MMMM Do YYYY, h:mm:ss a")}
             </span>
             <button
               className="blue-button px-5"
@@ -128,7 +129,7 @@ const OrderItem = ({ data, stateOrder, index }) => {
                 : "Reviews"}
             </button>
           </div>
-          <div className="col-span-2 flex items-center justify-center">
+          <div className="flex items-center justify-center">
             <Link href={`/order-detail/${data._id}`}>
               <HiOutlineChevronRight className=" w-11 h-11 p-2 text-gray-700 rounded-full  hover:bg-slate-300 hover:cursor-pointer duration-200" />
             </Link>
@@ -153,7 +154,7 @@ const OrderItem = ({ data, stateOrder, index }) => {
           />
         )}
       </Modal>
-    </>
+    </div>
   );
 };
 
