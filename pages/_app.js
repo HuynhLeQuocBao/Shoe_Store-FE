f441c80d58f1e589c0c616b36bcffebeebd27937
dao phan <daophan.pix@gmail.com>
2023-05-29 18:02:40 +0700

    format file app

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,61 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "../styles/globals.css";
-import { SessionProvider, getSession } from "next-auth/react";
-import { SWRConfig } from "swr";
-import { MainLayout } from "@/layout/index";
-import axiosClient from "../apiClient/axiosClient";
-import Head from "next/head";
 import App from "next/app";
-import { store, persistor } from "../store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { usePageLoading } from "hooks/usePageLoading";
-import LoadingPageGlobal from "@/components/section/loading/LoadingPageGlobal";
 import algoliasearch from "algoliasearch/lite";
 import { InstantSearch } from "react-instantsearch-dom";
-import { productApi } from "@/apiClient/product";
+import { SessionProvider, getSession } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../styles/globals.css";
+
+import { MainLayout } from "@/layout/index";
+import axiosClient from "../apiClient/axiosClient";
+import { store, persistor } from "../store/store";
+import { usePageLoading } from "hooks/usePageLoading";
+import LoadingPageGlobal from "@/components/section/loading/LoadingPageGlobal";
+import { productApi } from "@/apiClient/product";
+import { SWRConfig } from "swr";
 let productsCache;
+const HeadContent = () => (
+  <Head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Footwear </title>
+
+    <meta
+      name="description"
+      content="The purpose of this website is for the graduation thesis with the task of learning NextJS on the Front-end side and ExpressJS on the Back-end side to build an e-commerce website."
+    />
+    <meta name="keywords" content="shoe, shoe-store, bao, cat, kltn20" />
+    <meta name="author" content="Bao Huynh - Cat Le" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Footwear from KLTN 20" />
+    <meta property="og:site_name" content="Footwear from KLTN 20" />
+    <meta property="og:url" content="https://shoe-store-fe.vercel.app" />
+    <meta
+      property="og:description"
+      content="The purpose of this website is for the graduation thesis with the task of learning NextJS on the Front-end side and ExpressJS on the Back-end side to build an e-commerce website."
+    />
+    <meta
+      property="og:image"
+      content="https://shoe-store-fe.vercel.app/SEO_image.png"
+    />
+    <meta property="og:image:alt" content="Footwear from KLTN 20" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="567" />
+
+    <link rel="icon" type="image/x-icon" href="favicon.png" />
+  </Head>
+);
+
 function MyApp(props) {
   const { isPageLoading } = usePageLoading();
   const { Component, pageProps, session, navigationProps } = props;
@@ -32,39 +68,7 @@ function MyApp(props) {
 
   return (
     <>
-      <Head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        <title>Footwear </title>
-
-        <meta
-          name="description"
-          content="The purpose of this website is for the graduation thesis with the task of learning NextJS on the Front-end side and ExpressJS on the Back-end side to build an e-commerce website."
-        />
-        <meta name="keywords" content="shoe, shoe-store, bao, cat, kltn20" />
-        <meta name="author" content="Bao Huynh - Cat Le" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Footwear from KLTN 20" />
-        <meta property="og:site_name" content="Footwear from KLTN 20" />
-        <meta property="og:url" content="https://shoe-store-fe.vercel.app" />
-        <meta
-          property="og:description"
-          content="The purpose of this website is for the graduation thesis with the task of learning NextJS on the Front-end side and ExpressJS on the Back-end side to build an e-commerce website."
-        />
-        <meta
-          property="og:image"
-          content="https://shoe-store-fe.vercel.app/SEO_image.png"
-        />
-        <meta property="og:image:alt" content="Footwear from KLTN 20" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="567" />
-
-        <link rel="icon" type="image/x-icon" href="favicon.png" />
-      </Head>
-
+      <HeadContent />
       <SessionProvider session={session}>
         <SWRConfig
           value={{
@@ -92,10 +96,10 @@ function MyApp(props) {
           </Provider>
         </SWRConfig>
       </SessionProvider>
+      /
     </>
   );
 }
-
 MyApp.getInitialProps = async (context) => {
   const appProps = await App.getInitialProps(context);
 
@@ -117,5 +121,4 @@ MyApp.getInitialProps = async (context) => {
     },
   };
 };
-
 export default MyApp;
